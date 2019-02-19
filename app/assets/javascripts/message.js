a14e45295d9c5cef02d469c9a14f2cee2f02a510
zerotaisuke <taisuke@taisukenoMacBook-Pro.local>
2019-02-19 19:09:50 +0900

    非同期1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,64 @@
-$('.js-form').on('submit', function(){
-  console.log(hoge);
+     function buildHTML(message){
+      if ( message.image ) {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.date}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <asset_path src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.date}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$('.form__message').on('submit', function(){
+  e.preventDefault();
+  var formData = new formData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('messages')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
+  // console.log(hoge);
 });
